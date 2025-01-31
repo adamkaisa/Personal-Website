a2e11a8de2dd4934cc18ee8c65039a4fdb334067
Adam Khairusydan <75810467+adamkaisa@users.noreply.github.com>
2025-02-01 00:03:43 +0700

    Update Info.vue

--- src/components/About/Info.vue
+++ src/components/About/Info.vue
@@ -1,8 +1,9 @@
 <template>
-    <Card title="Who am I?" size="not-set">
+    <Card title="Who am I ?" size="not-set">
         I'm a self-taught developer from Jakarta/Indonesia. I'm currently as graduate college student, 
-        interested in every aspect of computer science, electronics, science and body building. To be honest, 
-        I'm a bit of a nerd. I love to learn and I always try to improve myself. I enjoy reading and watching about 
-        any topic but my favorites are sci-fi, psychology, history and self-improvement.
+        interested in every aspect of computer science, electronics and science. To be honest, 
+        I'm a bit of a nerd. I love to learn and I always try to improve myself. I enjoy listening japanese music 
+        and i've been self-learning japanese language more than 1 year. I love to watching about 
+        any topic but my favorites are sci-fi, psychology and history.
     </Card>
 </template>
