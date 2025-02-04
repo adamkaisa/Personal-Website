7d85ed0118ebf23374b48eede463bec6a060ce5c
Adam Khairusydan <75810467+adamkaisa@users.noreply.github.com>
2025-02-04 19:59:31 +0700

    Update Info.vue

--- src/components/About/Info.vue
+++ src/components/About/Info.vue
@@ -1,6 +1,6 @@
 <template>
     <Card title="Who am I ?" size="not-set">
-        I'm a self-taught developer from Jakarta/Indonesia. I'm currently as graduate college student, 
+        I'm a self-taught developer from Depok, West Java Indonesia. I'm currently as graduate college student, 
         interested in every aspect of computer science, electronics and science. To be honest, 
         I'm a bit of a nerd. I love to learn and I always try to improve myself. I enjoy listening japanese music 
         and i've been self-learning japanese language more than 1 year. I love to watching about 
